34c1368e8a54fb53e0e074f1c686df662454dd35
zammy-d <zammy.d.est@gmail.com>
2023-08-22 19:57:13 -0500

    Se crea index.php con estructura inicial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/margenSuperior.css"> <!-- Vincula tu archivo CSS aquí -->
+    <title>Mi Página con Márgenes</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1>Contenido con Margen</h1>
+        <p>Este es un ejemplo de un elemento con margen alrededor.</p>
+    </div>
+</body>
+</html>
